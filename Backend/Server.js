a1020df3f84b1c2784c9bdf0db758ac3f86b24a1
ahmad <ha5403905@gmail.com>
2023-05-09 15:43:32 +0500

    Connecting Database with Server

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { PORT } = require("./Config/Index");
+const dbConnect = require("./Database/Index");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}`);
